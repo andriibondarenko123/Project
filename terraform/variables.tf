353c56d1d3284b5d7ddda3a078e888ba4ed34e34
Oleh Tsiupak <tsyupak98@gmail.com>
2022-07-02 12:26:28 +0300

    update files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,12 +10,6 @@ variable avail_zone {
 variable env_prefix {
     default = "dev"
 }
-variable my_ip {
-    default = "92.253.236.163/32"
-}
-variable jenkins_ip {
-    default = "18.194.231.88/32"
-}
 variable instance_type {
     default = "t2.micro"
 }
